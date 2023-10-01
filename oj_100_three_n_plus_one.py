e464b5d291cf25b0dcdf2567f63b40952ceecb47
yaoamegandjin <yaoamegandjin@gmail.com>
2023-10-01 15:30:28 -0500

    made changes

--- oj_100_three_n_plus_one.py
+++ oj_100_three_n_plus_one.py
@@ -1,11 +1,21 @@
-def three_n_plus_one(i, j):
-    for n in range(-j):
-        if n == 1:
-            break
-        elif n % 2 == 0:
-            n/2
-            print(n)
+def cycle_length(n):
+    m = 1
+    while n != 1:
+        m += 1
+        if n % 2 == 0:
+            n = n / 2
         else:
-            n = 3 * n + 1
-            print(n)
-three_n_plus_one(1, 22)
+            n =  3 * n + 1
+    return m
+
+def maximum_cycle_length(i, j):
+    m = 0
+    for n in range(i, j + 1):
+        if m < cycle_length(n):
+            m = cycle_length(n)
+    return str(i) + " " + str(j) + " " + str(m)
+
+
+
+z = maximum_cycle_length(1, 10)
+print(z)
